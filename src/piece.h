64659ed53208b6f758938eb853765bc13825e11d
Efe <suleymanefeclk2@gmail.com>
2024-05-05 18:27:14 -0700

    🔧 style: remove unnecessary includes

--- src/piece.h
+++ src/piece.h
@@ -1,12 +1,9 @@
 #ifndef PIECE_H
 #define PIECE_H
 #include <map>
-#include <iostream>
 #include <vector>
 
-#include "board.h"
-
-static constexpr int PIECE_TYPE_MASK        = 0b00111;
+static constexpr int PIECE_TYPE_MASK = 0b00111;
 static constexpr int PIECE_WHITE_COLOR_MASK = 0b01000;
 static constexpr int PIECE_BLACK_COLOR_MASK = 0b10000;
 static constexpr int PIECE_COLOR_MASK = PIECE_WHITE_COLOR_MASK | PIECE_BLACK_COLOR_MASK;
@@ -35,19 +32,29 @@ public:
     static std::vector<int> legalMoves;
 
     static void loadPieceTextures();
+
     static bool isColor(const int &piece, const int &color);
+
     static int getColor(const int &piece);
+
     static int getType(const int &piece);
 
-    static void generateMoves(int piece, const int& squareIndex, const int board[]);
+    static void generateMoves(int piece, const int &squareIndex, const int board[]);
+
     static void generatePawnMoves(int piece, int rank, int file, const int board[]);
+
     static void generateRookMoves(int piece, int rank, int file, const int board[]);
+
     static void generateBishopMoves(int piece, int rank, int file, const int board[]);
+
     static void generateQueenMoves(int piece, int rank, int file, const int board[]);
+
     static void generateKnightMoves(int piece, int rank, int file, const int board[]);
+
     static void generateKingMoves(int piece, int rank, int file, const int board[]);
 
     static void generateLinearSlidingMoves(int piece, int rank, int file, bool isVertical, const int board[]);
+
     static void generateDiagonalSlidingMoves(int piece, int rank, int file, const int board[]);
 };
 
